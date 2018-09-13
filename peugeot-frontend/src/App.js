95e901852636195901e264311bf29d2073bde1d5
Le Trung Kien <kientrungle2001@gmail.com>
2018-09-13 18:44:12 +0700

    all

--- peugeot-frontend/src/App.js
+++ peugeot-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
+import { Alert } from 'reactstrap';
 
 import logo from './logo.svg';
 import './App.css';
@@ -16,6 +17,32 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
           <Button color="danger">Danger!</Button>
         </p>
+        <div>
+          <Alert color="primary">
+            This is a primary alert — check it out!
+            </Alert>
+                <Alert color="secondary">
+                  This is a secondary alert — check it out!
+            </Alert>
+                <Alert color="success">
+                  This is a success alert — check it out!
+            </Alert>
+                <Alert color="danger">
+                  This is a danger alert — check it out!
+            </Alert>
+                <Alert color="warning">
+                  This is a warning alert — check it out!
+            </Alert>
+                <Alert color="info">
+                  This is a info alert — check it out!
+            </Alert>
+                <Alert color="light">
+                  This is a light alert — check it out!
+            </Alert>
+                <Alert color="dark">
+                  This is a dark alert — check it out!
+            </Alert>
+        </div>
       </div>
     );
   }
