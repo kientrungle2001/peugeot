4625d26ea0e337f2bfd6386f8f5ce080040a4808
Le Trung Kien <kientrungle2001@gmail.com>
2018-09-13 19:17:12 +0700

    all

--- peugeot-frontend/src/App.js
+++ peugeot-frontend/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <section id="header-main">
-        <Container>
+        <Container class="header">
           Hello
         </Container>
       </section>
