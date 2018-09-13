63be9323cfe3de17a0f5b1085b7679cb7cdbbddf
Le Trung Kien <kientrungle2001@gmail.com>
2018-09-13 18:48:00 +0700

    all

--- peugeot-frontend/src/App.js
+++ peugeot-frontend/src/App.js
@@ -70,6 +70,21 @@ class App extends Component {
             This is a dark alert with <a href="#" className="alert-link">an example link</a>. Give it a click if you like.
       </Alert>
         </div>
+
+        <div>
+          <Alert color="success">
+            <h4 className="alert-heading">Well done!</h4>
+            <p>
+              Aww yeah, you successfully read this important alert message. This example text is going
+              to run a bit longer so that you can see how spacing within an alert works with this kind
+              of content.
+        </p>
+            <hr />
+            <p className="mb-0">
+              Whenever you need to, be sure to use margin utilities to keep things nice and tidy.
+        </p>
+          </Alert>
+        </div>
       </div>
     );
   }
