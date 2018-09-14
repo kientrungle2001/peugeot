4aa15776d76af40f356b10df05fcff62b56032bb
Le Trung Kien <kientrungle2001@gmail.com>
2018-09-14 10:45:03 +0700

    all

--- peugeot-frontend/src/App.js
+++ peugeot-frontend/src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
     return (
       <div>
         <Header />
-        <table>
+        <table className="table">
           <tbody>
             {this.state.data.map((person, i) => <TableRow key={i}
               data={person} />)}
