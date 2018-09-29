2318d005f1ff0e24f8a95e51218e88dc89125c4b
Le Trung Kien <kientrungle2001@gmail.com>
2018-09-29 09:47:42 +0700

    all

---
+++ peugeot-frontend/src/modules/Peugeot/Accessaries/Carousel.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+class ModulePeugeotAccessariesCarousel extends Component {
+    render() {
+        return (
+            <div className="section-full bg-img-fix dlab-new-work overlay-white-dark">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-3 col-sm-4  p-a0 no-of-item">
+                            <div className="no-of-item-dtl">
+                                <img src="/img/Peugeot-logo.png" alt="Peugeot Logo" />
+                                <h3>Phụ tùng theo xe</h3>
+                            </div>
+                        </div>
+                        <div className="col-md-9 col-sm-8 p-a0">
+                            <div className="new-item owl-btn-style-1 owl-carousel">
+                                <div className="item dlab-new-item">
+                                    <div className="dlab-box">
+                                        <div className="dlab-media">
+                                            <a href="#"><img src="/img/pic1a.jpg" alt="" /></a>
+                                        </div>
+                                        <div className="dlab-info">
+                                            <p className="event-date">Phụ tùng</p>
+                                            <h4 className="dlab-title"><a href="#">Peugeot 5008 <i className="fa fa-angle-right pull-right"></i></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item dlab-new-item">
+                                    <div className="dlab-box">
+                                        <div className="dlab-media">
+                                            <a href="#"><img src="/img/pic2a.jpg" alt="" /></a>
+                                        </div>
+                                        <div className="dlab-info">
+                                            <p className="event-date">Phụ tùng</p>
+                                            <h4 className="dlab-title"><a href="#">Peugeot 5008 <i className="fa fa-angle-right pull-right"></i></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item dlab-new-item">
+                                    <div className="dlab-box">
+                                        <div className="dlab-media">
+                                            <a href="#"><img src="/img/pic3a.jpg" alt="" /></a>
+                                        </div>
+                                        <div className="dlab-info">
+                                            <p className="event-date">Phụ tùng</p>
+                                            <h4 className="dlab-title"><a href="#">Peugeot 5008 <i className="fa fa-angle-right pull-right"></i></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="item dlab-new-item">
+                                    <div className="dlab-box">
+                                        <div className="dlab-media">
+                                            <a href="#"><img src="/img/pic2a.jpg" alt="" /></a>
+                                        </div>
+                                        <div className="dlab-info">
+                                            <p className="event-date">Phụ tùng</p>
+                                            <h4 className="dlab-title"><a href="#">Peugeot 5008 <i className="fa fa-angle-right pull-right"></i></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ModulePeugeotAccessariesCarousel;
